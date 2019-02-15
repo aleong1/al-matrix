5f954dfd08be8ecc79645ce980aa53833c70542a
Alexia Leong <aleong1@stuy.edu>
2019-02-15 11:43:52 -0500

    it's fridayyyyyy

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ matrix = new_matrix()
 
 draw_lines( matrix, screen, color )
 display(screen)
+
+
+#functions to test:
+print_matrix( matrix )
+ident( matrix )
+matrix_mult( m1, m2 )
+
+draw_lines( matrix, screen, color )
+add_edge( matrix, x0, y0, z0, x1, y1, z1 )
+add_point( matrix, x, y, z=0 )
